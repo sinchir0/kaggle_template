826fb8fa04cc553b15babbccf8ad9fe28ddd7d14
sinchir0 <dekunattou@gmail.com>
2024-08-13 17:09:51 +0900

    add

--- {{cookiecutter.competition_name}}/shell/set_kaggle_api.sh
+++ {{cookiecutter.competition_name}}/shell/set_kaggle_api.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p ~/.kaggle/
-cp {{cookiecutter.competition_name}}/kaggle.json ~/.kaggle/
+cp env_file/kaggle.json ~/.kaggle/
 chmod 600 ~/.kaggle/kaggle.json
 
 pip install kaggle
